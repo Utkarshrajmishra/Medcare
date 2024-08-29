3877c0814214b88168e17ae71c8b944a69b5f248
Utkarsh Raj Mishra <utkarshrajmishra811545@gmail.com>
2024-08-29 21:07:05 +0530

    Add: NavBar Component

---
+++ src/components/NavBar.jsx
@@ -0,0 +1,44 @@
+import Logo from "../assets/images/logo.png";
+
+const NavLinks = [
+  {
+    id: 1,
+    links: "Home",
+  },
+  {
+    id: 2,
+    links: "Prediction",
+  },
+  {
+    id: 3,
+    links: "About",
+  },
+  {
+    id: 4,
+    links: "Doctor Appointment",
+  },
+];
+
+const NavBar = () => {
+  return (
+    <nav className="py-5 ">
+      <section className="flex items-center text-textColor justify-between px-16">
+        <div>
+          <img src={Logo} alt="wesbite logo" />
+        </div>
+        <ul className="flex items-center gap-[2.7rem] text-[0.96rem]">
+          {NavLinks?.map((items, index) => (
+            <li key={items.id} className="hover:text-primaryColor">
+              {items.links}
+            </li>
+          ))}
+        </ul>
+        <button className="bg-primaryColor px-5 text-[0.9rem] text-white py-1 rounded-3xl">
+          Login
+        </button>
+      </section>
+    </nav>
+  );
+};
+
+export default NavBar;
