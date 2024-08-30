64314d808aae3e5be50fa4c19bc4c84048044bdb
Utkarsh Raj Mishra <utkarshrajmishra811545@gmail.com>
2024-08-30 21:07:38 +0530

    Add: Prediction Page

--- src/components/SearchBox.jsx
+++ src/components/SearchBox.jsx
@@ -22,7 +22,7 @@ const SearchBox = () => {
         <input
           type="search"
           id="default-search"
-          className="block w-full p-4 pl-10 text-sm font-inter text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+          className="block w-full p-4 pl-10 text-sm font-inter outline-1 outline-blue-100 outline  border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500  d dark:focus:ring-blue-500 dark:focus:border-blue-500"
           placeholder="Search problems by name"
           required
         />
--- src/pages/Prediction.jsx
+++ src/pages/Prediction.jsx
@@ -1,20 +1,64 @@
+import { useState } from "react";
 import NavBar from "../components/NavBar";
+import SearchBox from "../components/SearchBox";
+import { RxCross2 } from "react-icons/rx";
+import { Disease } from "../constants/ Diseases";
 
 const Prediction = () => {
+  const [selectedDisease, setSelectedDisease] = useState([]);
+
+  const addItem = (index) => {
+    const disease = Disease[index];
+    if (!selectedDisease.some((item) => item.ID === disease.ID)) {
+      setSelectedDisease((prevState) => [...prevState, disease]);
+    }
+  };
+
+  const removeItem = (index) => {
+    const disease = [...selectedDisease];
+    disease.splice(index, 1);
+    setSelectedDisease(disease);
+  };
+
   return (
     <section>
       <NavBar />
-      <section className="md:px-20 px-10 mt-8">
-        <div className="flex flex-col gap-2 items-center">
-          <p className="text-2xl font-[500]">Disease Prediction</p>
-          <p className="text-[0.9rem] text-textColor text-center">
-            Welcome to our Disease Prediction page! Our cutting-edge tool
-            leverages the power of Generative AI to provide accurate and timely
-            predictions of potential health risks based on your symptoms,
-            medical history, and other relevant factors.
-          </p>
-        </div>
-        
+      <section className="md:px-20 px-5  py-8">
+        <SearchBox />
+        {/* Selected Diseases Section */}
+        {selectedDisease.length > 0 && (
+          <section className="mt-8">
+            <p className="text-xl font-semibold mb-4">Selected Diseases</p>
+            <div className="flex overflow-x-auto space-x-4 py-2 px-1">
+              {selectedDisease.map((item, index) => (
+                <p
+                  key={item.ID}
+                  onClick={() => removeItem(index)}
+                  className="py-2  cursor-pointer px-2 bg-blue-100 text-blue-600 outline outline-1  outline-blue-700 text-sm rounded-3xl flex items-center justify-center h-full"
+                >
+                  {item.Name} <RxCross2 className="ml-2 text-xl" />
+                </p>
+              ))}
+            </div>
+            <button className="bg-blue-500 font-bold h-10 w-full rounded-md text-white">
+              Continue
+            </button>
+          </section>
+        )}
+        {/* All Diseases Section */}
+        <section className="mt-8">
+          <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-4">
+            {Disease.map((item, index) => (
+              <div
+                key={item.ID}
+                onClick={() => addItem(index)}
+                className="py-2 px-1 bg-gray-50 outline outline-1 text-gray-600 outline-blue-100 text-sm rounded-3xl flex items-center justify-center h-full"
+              >
+                <p className="text-center cursor-pointer">{item.Name}</p>
+              </div>
+            ))}
+          </div>
+        </section>
       </section>
     </section>
   );
