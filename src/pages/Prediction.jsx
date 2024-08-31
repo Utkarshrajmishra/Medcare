5b492153fc7ec21caf0b7f7d91ea9710d324e96e
Utkarsh Raj Mishra <utkarshrajmishra811545@gmail.com>
2024-08-31 22:59:21 +0530

    Fixed: Prediction Page UI

--- src/pages/Prediction.jsx
+++ src/pages/Prediction.jsx
@@ -4,7 +4,19 @@ const Prediction = () => {
   return (
     <>
       <NavBar />
-      <section className="px-20 mt-10">
+      <section className="px-20 mt-10 flex flex-col gap-8">
+        <div className="flex flex-col gap-2 items-center">
+          <p className="text-2xl text-gray-800 font-semibold">
+            Disease Predicted: Your Health Insights Report
+          </p>
+          <p className="text-md text-center text-textColor text-">
+            Below, you will find a list of diseases and conditions that our
+            system has identified as potential risks for you. Each prediction
+            includes an estimated likelihood, key factors contributing to the
+            risk, and actionable recommendations to help you manage or reduce
+            these risks.
+          </p>
+        </div>
         <Table />
       </section>
     </>
