dffd52d5c0f12e8d7c964715dbeaf27ba77b2601
Utkarsh Raj Mishra <utkarshrajmishra811545@gmail.com>
2024-10-04 20:06:24 +0530

    Add: Signup Component

--- src/pages/Auth.jsx
+++ src/pages/Auth.jsx
@@ -1,11 +1,12 @@
 import toast from "react-hot-toast";
 import Login from "@/components/Login";
+import Signup from "@/components/Signup";
 import NavBar from "@/components/NavBar";
 const Auth = () => {
   return (
     <>
       <NavBar />
-      <Login />
+      <Signup />
     </>
   );
 };
